ea1fb993175a4ea1725aeaf6f8df8bcf417da25a
Mobbstar <Mobbstar@users.noreply.github.com>
2026-02-27 07:55:42 +0100

    fix crash if anyone else pickpockets

--- scripts/monkeypatches/stealCredits.lua
+++ scripts/monkeypatches/stealCredits.lua
@@ -5,7 +5,7 @@ local stealCredits = abilitydefs.lookupAbility("stealCredits")
 local executeAbility = stealCredits.executeAbility
 stealCredits.executeAbility = function(self, sim, unit, userUnit, ...)
     local ability = userUnit and userUnit:ownsAbility("luna4s_cloak")
-    if ability and #unit:getChildren() == 0 or not simquery.canSteal(sim, userUnit, unit) then -- itemless
+    if ability and (#unit:getChildren() == 0 or not simquery.canSteal(sim, userUnit, unit)) then -- itemless
         ability:addCharge(sim, 1)
         ability:refreshCloakDuration()
     end

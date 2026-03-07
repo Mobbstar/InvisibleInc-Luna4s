a56a53cf934216296cc683471c1796fbb6862245
pptsqd <123323477+pptsqd@users.noreply.github.com>
2026-03-07 20:54:27 +0000

    Update simquery.lua
    We don't need to check the player, and it's less confusing to return 'true' for 'is_silenced'

--- scripts/monkeypatches/simquery.lua
+++ scripts/monkeypatches/simquery.lua
@@ -4,7 +4,7 @@ local mathutil = include("modules/mathutil")
 local canHear = simquery.canHear
 simquery.canHear = function(unit, ...)
     local result = {canHear(unit, ...)}
-    if result[1] and unit and not simquery.luna4s_isUnitSilenced(unit:getSim(), unit) then
+    if result[1] and unit and simquery.luna4s_isUnitSilenced(unit:getSim(), unit) then
         return false
     end
     return unpack(result)
@@ -13,22 +13,39 @@ end
 simquery.luna4s_isUnitSilenced = function(sim, hearingUnit, x1, y1)
     assert(hearingUnit)
     local x0, y0 = hearingUnit:getLocation()
-    local player = hearingUnit:getPlayerOwner()
 
     for i, silencerUnit in pairs(sim:getAllUnits()) do
-        if silencerUnit and silencerUnit:getTraits().luna4s_silenceRadius and silencerUnit:getPlayerOwner() ~= player then
+        if silencerUnit and silencerUnit:getTraits().luna4s_silenceRadius then
             local silenceRadius = silencerUnit:getTraits().luna4s_silenceRadius
             local x2, y2 = silencerUnit:getLocation()
             if x2 and y2 then
                 if mathutil.dist2d(x0, y0, x2, y2) <= silenceRadius then
-                    return false
+                    return true
                 end
                 if x1 and y1 and mathutil.dist2d(x1, y1, x2, y2) <= silenceRadius then
-                    return false
+                    return true
                 end
             end
         end
     end
 
-    return true
+    return false
+end
+
+
+simquery.luna4s_isTileSilenced = function(sim, x0, y0)
+
+    for i, silencerUnit in pairs(sim:getAllUnits()) do
+        if silencerUnit and silencerUnit:getTraits().luna4s_silenceRadius then
+            local silenceRadius = silencerUnit:getTraits().luna4s_silenceRadius
+            local x2, y2 = silencerUnit:getLocation()
+            if x2 and y2 then
+                if mathutil.dist2d(x0, y0, x2, y2) <= silenceRadius then
+                    return true
+                end
+            end
+        end
+    end
+
+    return false
 end
